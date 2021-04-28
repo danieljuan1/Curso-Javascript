102797a66e93e2364dc16a2b7945beb1123d6744
daniel juan <danieleduard1994@gmail.com>
2021-04-27 18:11:54 -0700

    inicia el curso javascript

---
+++ javascript.js
@@ -0,0 +1,24 @@
+function diaActual(){
+    var hoy=new Date();
+    document.write(hoy.toDateString());
+}
+
+document.write("<br/>")
+
+function aviso(){
+    alert("hola")
+}
+
+function evento(){
+    alert("has dado click a la imagen");
+}
+
+function mapa(){
+    var foto = document.getElementsByTagName("img")[0];
+    foto.addEventListener("click", evento, false);
+}
+
+window.onload=mapa;
+
+var anuncio=document.getElementsByClassName("anuncio");
+anuncio.innerHTML="VENDE";
